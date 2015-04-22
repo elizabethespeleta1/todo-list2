c9c3473db59a0a601b4f6726b7c932948eb5142f
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-04-21 17:41:43 -0700

    commented on 6

--- index.php
+++ index.php
@@ -5,15 +5,20 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<!-- -->
+	<!-- how to add items for to-do list -->
 	<div class="wrap">
+		<!-- unordered list -->
 		<div class="task-list">
 		<ul>
+			<!-- getting this file because it needs to connect first -->
 			<?php require("includes/connect.php");?>
 		</ul>
-		
 		</div>
-
+	<!-- form  -->
+	<form class="add-new-task" autocomplete="off">
+		
+		<input type="text" name="new-task" placeholder="add new iten..."/>
+	</form>
 	</div>
 
 </body>
