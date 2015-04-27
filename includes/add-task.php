868a19a847c0261cdff633c93ce746cee5f65886
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-04-27 06:55:00 -0700

    commented on 11

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,12 +1,17 @@
 <?php 
+	//making new variables
 	$task = strip_tags($_POST['task']);
 	$date = $date('Y-m-d');
 	$time = date('H:i:s');
 
+	//connecting to database
 	include('connect.php');
 
+	//putting info in mysqli
+	//then mysqli to put things into the database
 	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 	$mysqli ->query("INSERT INTO tasks VALUES('','$task','$date','$time')");
 
+	//actually querying the tasks
 	$query = "SELECT = FROM tasks WHERE task='$task' and date='$date' and time='$time'";
 ?>
