6637addab0872d0028a12de68d0b4f4705f4d105
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-04-27 12:02:13 -0700

    finished video 14

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -3,10 +3,11 @@
 	//getting connect file
 	require('connect.php');
 	//logging into localhost
-	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 
 	//if the result = the database delete the task
 	if($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")){
 
 	}
+	
  ?>
