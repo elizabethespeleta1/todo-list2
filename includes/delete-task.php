e60f8edc6606ca668961737c665c31495bead7a4
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-04-27 07:10:06 -0700

    commented on 13

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -1,9 +1,12 @@
 <?php 
 	$task_id = $strip_tags($_POST['id']);
+	//getting connect file
 	require('connect.php');
+	//logging into localhost
 	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 
+	//if the result = the database delete the task
 	if($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")){
-		
+
 	}
  ?>
